066f0138bd4b8309cd619310b9654d3cbfa03d32
Philip Harmash <infilya@ukr.net>
2024-05-20 19:23:58 -0400

    now

--- src/ratings/components/Rows.tsx
+++ src/ratings/components/Rows.tsx
@@ -36,14 +36,32 @@ export function Rows(props: TRows) {
           <td>
             <img src={`/photos/${"team" in player ? player.team : player.name}.png`} alt="" />
           </td>
-          <td style={lastRow ? { backgroundColor: "yellow" } : {}}>{player.leftInGame}</td>
-          <td style={lastRow ? { backgroundColor: "orange" } : {}}>{player.attacksInBlock}</td>
-          <td style={lastRow ? { backgroundColor: "orangered" } : {}}>{player.loosePoints}</td>
-          <td style={lastRow ? { backgroundColor: "lightgreen" } : {}}>{player.winPoints}</td>
-          <td style={lastRow ? { backgroundColor: "orangered" } : {}}>{player.serviceFailed}</td>
-          <td style={lastRow ? { backgroundColor: "orange" } : {}}>{player.serviceMinus}</td>
-          <td style={lastRow ? { backgroundColor: "yellow" } : {}}>{player.servicePlus}</td>
-          <td style={lastRow ? { backgroundColor: "lightgreen" } : {}}>{player.ace}</td>
+          <td style={lastRow ? { backgroundColor: "orangered" } : { backgroundColor: "khaki" }}>
+            {player.serviceFailed}
+          </td>
+          <td style={lastRow ? { backgroundColor: "orange" } : { backgroundColor: "khaki" }}>
+            {player.serviceMinus}
+          </td>
+          <td style={lastRow ? { backgroundColor: "yellow" } : { backgroundColor: "khaki" }}>
+            {player.servicePlus}
+          </td>
+          <td style={lastRow ? { backgroundColor: "lightgreen" } : { backgroundColor: "khaki" }}>
+            {player.ace}
+          </td>
+          <td style={lastRow ? { backgroundColor: "yellow" } : { backgroundColor: "gainsboro" }}>
+            {player.leftInGame}
+          </td>
+          <td style={lastRow ? { backgroundColor: "orange" } : { backgroundColor: "gainsboro" }}>
+            {player.attacksInBlock}
+          </td>
+          <td style={lastRow ? { backgroundColor: "orangered" } : { backgroundColor: "gainsboro" }}>
+            {player.loosePoints}
+          </td>
+          <td
+            style={lastRow ? { backgroundColor: "lightgreen" } : { backgroundColor: "gainsboro" }}
+          >
+            {player.winPoints}
+          </td>
           <td style={setStyleForEfficency(getAttackEfficency(player))}>
             {getAttackEfficency(player)} %
           </td>
